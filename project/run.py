6d9567209d749bc5222d948520e8d0fdd05fe2da
Toigonbai Turgunbaev <turgunbaevt@gmail.com>
2020-01-09 22:51:07 +0600

    login completed

---
+++ project/run.py
@@ -0,0 +1,5 @@
+from views import app
+
+app.run(debug=True)
+
+
--- project/views.py
+++ project/views.py
@@ -1,40 +1,41 @@
 import sqlite3
+from flask import Flask, flash, render_template, request, redirect, url_for, g, session
 from functools import wraps
-from flask import Flask, flash, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 app.config.from_object('_config')
 
 
 def connect_db():
-    return sqlite3.connect(app.config["DATABASE_PATH"])
+    return sqlite3.connect(app.config['DATABASE_PATH'])
 
 
 def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if 'logged_in' in session:
+        if "logged_in" in session:
             return func(*args, **kwargs)
         else:
-            flash("You need to login first")
+            flash("Authentication is required, please login")
             return redirect(url_for('login'))
     return wrapper
 
 
-@app.route('/logout/')
-def logout():
-    session.pop("logged_in", None)
-    flash('Goodbye!!!')
-    return redirect(url_for('login'))
-
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods = ["GET", "POST"])
 def login():
-    if request.methods == "POST":
-        if request.form['username'] != app.config["USERNAME"] or request.form["password"] != app.config["PASSWORD"]:
-            error = "Invalid user credentials"
-            return render_template('login_html', error=error)
+    if request.method == "POST":
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = "Invalid user credentials, please try again"
+            return render_template('logint.html', error=error)
         else:
             session['logged_in'] = True
-            flash('Welcome')
+            flash('Welcome!!!')
             return redirect(url_for('tasks'))
     return render_template('login.html')
+
+
+@app.route('/logout/')
+def logout():
+    session.pop('logged_in', None)
+    flash('Goodbye!!!')
+    return redirect(url_for('login'))
